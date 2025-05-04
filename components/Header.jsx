f746ef9dc277f7e1c70211871d64977506334d95
Reyansh Kumar <rexnoah0667@gmail.com>
2025-05-04 21:06:55 +0530

    `Replace Image component with img tag in Header component`

--- components/Header.jsx
+++ components/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
             </svg>
           </button>
           <div onClick={() => router.push("/")} className="cursor-pointer">
-            <Image src="/urban.jpg" alt="logo" width={100} height={30} className="animate-pulse" />
+            <img src="/urban.jpg" alt="logo" width={100} height={30} className="animate-pulse" />
           </div>
         </div>
 
